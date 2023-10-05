6f1ed6edca65c8f40631e91d047cbaccfb3bdb46
siggu <m0_w_0m@naver.com>
2023-10-06 00:07:14 +0900

    Spring Security(JWT Provider)

---
+++ board-back/src/main/java/com/project/boardback/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.project.boardback.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.project.boardback.provider.JwtProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  
+  private final JwtProvider jwtProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+
+
+  }
+
+  private String parseBearerToken(HttpServletRequest request) {
+    
+    String authorization = request.getHeader("Authorization");
+
+    boolean hasAuthorization = StringUtils.hasText(authorization);
+    if (!hasAuthorization) return null;
+
+    boolean isBearer = authorization.startsWith("Bearer ");
+    if (!isBearer) return null;
+
+    String token = authorization.substring(7);
+    return token;
+
+  }
+
+}
