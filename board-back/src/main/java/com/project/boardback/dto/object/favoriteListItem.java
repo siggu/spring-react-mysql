ce8f02140aa4b86ac07afc3435577707e51a34de
siggu <m0_w_0m@naver.com>
2023-10-06 16:29:53 +0900

    interface

---
+++ board-back/src/main/java/com/project/boardback/dto/object/favoriteListItem.java
@@ -0,0 +1,14 @@
+package com.project.boardback.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class favoriteListItem {
+  private String email;
+  private String nickname;
+  private String profileImage;
+}
