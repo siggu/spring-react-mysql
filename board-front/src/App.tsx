8a004f4ee72bcb4588222e233405cbc377ae62c9
siggu <m0_w_0m@naver.com>
2023-10-06 23:09:28 +0900

    Create Component

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import BoardItem from "components/BoardItem";
+import latestBoardListMock from "mocks/latest-board-list-mock";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {latestBoardListMock.map((boardListItem) => (
+        <BoardItem boardListItem={boardListItem} />
+      ))}
+    </>
+  );
 }
 
 export default App;
---
+++ board-front/src/mocks/latest-board-list-mock.ts
@@ -0,0 +1,58 @@
+import { BoardListItem } from "types/interface";
+
+const latestBoardListMock: BoardListItem[] = [
+  {
+    boardNumber: 1,
+    title: "제목",
+    content: "내용",
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: "2023. 10. 06",
+    writerNickname: "김정목",
+    writerProfileImage:
+      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+  },
+  {
+    boardNumber: 1,
+    title: "제목",
+    content: "내용",
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: "2023. 10. 06",
+    writerNickname: "김정목",
+    writerProfileImage:
+      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+  },
+  {
+    boardNumber: 1,
+    title: "제목",
+    content: "내용",
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: "2023. 10. 06",
+    writerNickname: "김정목",
+    writerProfileImage:
+      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+  },
+  {
+    boardNumber: 1,
+    title: "제목",
+    content: "내용",
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: "2023. 10. 06",
+    writerNickname: "김정목",
+    writerProfileImage:
+      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+  },
+];
+
+export default latestBoardListMock;
