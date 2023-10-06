7bff2080d4ce11f72fdf3050d780a0753db09363
siggu <m0_w_0m@naver.com>
2023-10-06 23:44:50 +0900

    Create Component

--- board-front/src/mocks/latest-board-list-mock.ts
+++ board-front/src/mocks/latest-board-list-mock.ts
@@ -5,53 +5,57 @@ const latestBoardListMock: BoardListItem[] = [
     boardNumber: 1,
     title: "제목",
     content: "내용",
-    boardTitleImage: null,
+    boardTitleImage:
+      "https://cdn.eyesmag.com/content/uploads/posts/2022/03/21/gyeongbokgung-palace-night-01-1408c7c5-b5f5-4554-b3dc-7f3be6f75acc.jpg",
     favoriteCount: 0,
     commentCount: 0,
     viewCount: 0,
     writeDatetime: "2023. 10. 06",
     writerNickname: "김정목",
     writerProfileImage:
-      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+      "https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg",
   },
   {
     boardNumber: 1,
     title: "제목",
     content: "내용",
-    boardTitleImage: null,
+    boardTitleImage:
+      "https://cdn.eyesmag.com/content/uploads/posts/2022/03/21/gyeongbokgung-palace-night-01-1408c7c5-b5f5-4554-b3dc-7f3be6f75acc.jpg",
     favoriteCount: 0,
     commentCount: 0,
     viewCount: 0,
     writeDatetime: "2023. 10. 06",
     writerNickname: "김정목",
     writerProfileImage:
-      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+      "https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg",
   },
   {
     boardNumber: 1,
     title: "제목",
     content: "내용",
-    boardTitleImage: null,
+    boardTitleImage:
+      "https://cdn.eyesmag.com/content/uploads/posts/2022/03/21/gyeongbokgung-palace-night-01-1408c7c5-b5f5-4554-b3dc-7f3be6f75acc.jpg",
     favoriteCount: 0,
     commentCount: 0,
     viewCount: 0,
     writeDatetime: "2023. 10. 06",
     writerNickname: "김정목",
     writerProfileImage:
-      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+      "https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg",
   },
   {
     boardNumber: 1,
     title: "제목",
     content: "내용",
-    boardTitleImage: null,
+    boardTitleImage:
+      "https://cdn.eyesmag.com/content/uploads/posts/2022/03/21/gyeongbokgung-palace-night-01-1408c7c5-b5f5-4554-b3dc-7f3be6f75acc.jpg",
     favoriteCount: 0,
     commentCount: 0,
     viewCount: 0,
     writeDatetime: "2023. 10. 06",
     writerNickname: "김정목",
     writerProfileImage:
-      "https://www.hanmk.com/news/photo/202306/16714_40371_5250.jpg",
+      "https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg",
   },
 ];
 
