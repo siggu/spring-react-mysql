b62af46a43282377f4109c888c8cb59326894b0f
siggu <m0_w_0m@naver.com>
2023-10-08 22:21:36 +0900

    React-Router-Dom SPA

---
+++ board-front/src/layouts/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+//         component: 헤더 레이아웃          //
+export default function Header() {
+  //         render: 헤더 레이아웃 렌더링          //
+  return <div>Header</div>;
+}
